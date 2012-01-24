f720721df5277d6525475fa8885c8626ae6bc5de
Andrew Heiss <andrewheiss@gmail.com>
2012-01-23 22:27:15 -0700

    Added homework 5

---
+++ PMGT 634 homework/Homework 5.R
@@ -0,0 +1,20 @@
+library(ggplot2)
+library(timeDate)
+
+# Part a
+x <- seq(-5,40,by=.5)
+y <- dchisq(x,df=10)
+qplot(x, y, main="Probability density function for the Chi-square distribution with 10 degrees of freedom\n")
+
+# Part b
+# Generate a random sample of 50 observations
+set.seed(12345)     # Just for consistency's sake
+sample <- rchisq(50, df=10)
+
+# Part c
+round(mean(sample), 3)
+round(median(sample), 3)
+round(sd(sample), 3)
+round(IQR(sample), 3)
+round(skewness(sample), 3)
+round(kurtosis(sample, method=c("excess")), 3)
